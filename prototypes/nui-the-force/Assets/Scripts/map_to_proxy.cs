fe22b6623c91fe7073d53b85db0131678b0c9e04
Marco Pagani <Marco-Pagani@users.noreply.github.com>
2020-02-12 18:35:59 -0500

    set up object mapping

---
+++ prototypes/nui-the-force/Assets/Scripts/map_to_proxy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class map_to_proxy : MonoBehaviour
+{
+public Transform mainScene, copyFrom, copyTo ;
+public float scaleFactor = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject proxy = Instantiate(gameObject);
+        createProxyScene();
+    }
+
+    void createProxyScene(){
+        //copyTo.scale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+        foreach (Transform child in mainScene){
+            var proxyPos = child.InverseTransformPoint(copyFrom.position) * scaleFactor;
+            GameObject proxyObj = Instantiate(child, copyTo).gameObject;
+            proxyObj.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+            var proxyComp = proxyObj.AddComponent<proxy>() as proxy;
+            proxyComp.target = child;
+            proxyComp.scale = scaleFactor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ prototypes/nui-the-force/Assets/Scripts/proxy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proxy : MonoBehaviour
+{
+
+    public Transform target;
+    public float scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target.localPosition = gameObject.transform.localPosition * (1/scale);
+        
+    }
+}

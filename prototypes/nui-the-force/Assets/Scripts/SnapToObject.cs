a0b44b2984a9b6493a844521b36eaab5a78c977c
Isaac Wang <wangi@ufl.edu>
2020-02-17 13:26:17 -0500

    made an integration scene that combines the Meta/Leap interactions with object mapping

---
+++ prototypes/nui-the-force/Assets/Scripts/SnapToObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToObject : MonoBehaviour
+{
+	public GameObject root;
+	public string name;
+	
+	private Transform destination;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destination == null)
+		{
+			Transform obj = root.transform.Find(name);
+			if (obj != null)
+			{
+				//transform.position = obj.position;
+				//transform.parent = obj;
+				destination = obj;
+			}
+		}
+		
+		if (destination != null)
+		{
+			transform.position = destination.position + new Vector3(0f, 0.2f, 0f);
+		}
+    }
+}

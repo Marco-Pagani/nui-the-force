27e70999561e010d32d2edb3c42e50352b0e741c
Isaac Wang <wangi@ufl.edu>
2020-02-22 12:41:57 -0500

    Add grasp to clone interaction

---
+++ prototypes/nui-the-force/Assets/Scripts/HandGestureManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGestureManager : MonoBehaviour
+{
+    public map_to_proxy proxyManager;
+
+    bool active = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (proxyManager == null)
+        {
+            proxyManager = FindObjectOfType<map_to_proxy>();
+        }
+    }
+
+    public void StartGrasp()
+    {
+        if (!active)
+        {
+            proxyManager.createProxyScene();
+            active = true;
+        }
+    }
+
+    public void EndGrasp()
+    {
+        if (active)
+        {
+            proxyManager.deleteProxyScene();
+            active = false;
+        }
+    }
+}

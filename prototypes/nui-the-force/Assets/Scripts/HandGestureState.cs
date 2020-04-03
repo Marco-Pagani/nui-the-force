fb5c19328eebd4b930c74741daa769dfcb4ab537
Isaac Wang <wangi@ufl.edu>
2020-04-03 13:25:34 -0400

    Update palm direction recognition

--- prototypes/nui-the-force/Assets/Scripts/HandGestureState.cs
+++ prototypes/nui-the-force/Assets/Scripts/HandGestureState.cs
@@ -14,6 +14,11 @@ public class HandGestureState : MonoBehaviour
     public float graspOnThreshold = 0.9f;
     public float graspOffThreshold = 0.5f;
     public float graspPercent = 0;
+
+    public float palmUpThreshold = 80f;
+    public float palmDownThreshold = 90f;
+    public float palmAngle = 0; // Up: 0 deg, Down: 180 deg (normalized both ways)
+
     public bool isGrasp = false;
 	public bool palmUp = false;
 
@@ -35,30 +40,54 @@ public class HandGestureState : MonoBehaviour
 		OnGraspChanged?.Invoke(false);
     }
 
+    public void StartPalmUp()
+    {
+        Debug.Log("PalmUp");
+        palmUp = true;
+        OnPalmChanged?.Invoke(true);
+    }
+
+    public void EndPalmUp()
+    {
+        Debug.Log("PalmDown");
+        palmUp = true;
+        OnPalmChanged?.Invoke(false);
+    }
+
 	public void OnDirectionActivate()
 	{
+        // This code is leftover from the built-in Leap recognition (which is unreliable)
+
+        /*
 		//Debug.Log("PalmUp");
 		palmUp = true;
 		OnPalmChanged?.Invoke(true);
+        */
     }
 
 	public void OnDirectionDeactivate()
 	{
+        // This code is leftover from the built-in Leap recognition (which is unreliable)
+
+        /*
 		//Debug.Log("PalmDown");
 		palmUp = false;
         OnPalmChanged?.Invoke(false);
-	}
+        */
+    }
 
-	private void Update() {
+    private void Update() {
 		if (!hand.isTracked)
 		{
             // Reset grasp (sometimes gets out of sync)
             graspPercent = 0;
+            palmAngle = 180;
         }
 		else
 		{
-            // New method to check for hand grasp
+            // Check for recognition grap and palm up
             graspPercent = Mathf.Min(hand.leapHand.GrabAngle / Mathf.PI, 1);
+            palmAngle = 180 - Mathf.Abs(180 - (hand.leapHand.PalmNormal.Roll % 360)); // Normalize to [0, 180]
         }
 
         if (graspPercent > graspOnThreshold)
@@ -68,6 +97,15 @@ public class HandGestureState : MonoBehaviour
         else if (graspPercent < graspOffThreshold)
         {
             EndGrasp();
+        }
+
+        if (palmAngle < palmUpThreshold)
+        {
+            StartPalmUp();
+        }
+        else if (palmAngle > palmDownThreshold)
+        {
+            EndPalmUp();
         }
 	}
 }

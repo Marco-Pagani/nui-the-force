2d7d1b59b7f77c742f1d866d1d65954c9baf45fa
Isabel Laurenceau <isalau@ufl.edu>
2020-04-06 11:40:08 -0400

    Transparency Interaction
    So everything changes transparencies. Now I need to make sure
    A) It's envoked when the actual copy is made
    
    B) The selected item doesn't turn transparent

--- prototypes/nui-the-force/Assets/transparentSelect.cs
+++ prototypes/nui-the-force/Assets/transparentSelect.cs
@@ -2,41 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class transparentSelect : MonoBehaviour
-{
+public class transparentSelect : MonoBehaviour{
+  private GameObject sphere;
+  private Renderer render;
+  private Color tempcolor;
 
     // Start is called before the first frame update
-    void Start()
-    {
-
+    void Start(){
+      Debug.Log("transparency script");
+      render = gameObject.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+      GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
 
-    }
-
-    private void FindAll(){
-        Object[] tempList = Resources.FindObjectsOfTypeAll(typeof(GameObject));
-        List<GameObject> realList = new List<GameObject>();
-        GameObject temp;
-        Color tempcolor;
-
-        foreach(Object obj in tempList){
-            if(obj is GameObject){
-                temp = (GameObject)obj;
-                if(temp.hideFlags == HideFlags.None)
-
-                //change the opacity
-                tempcolor = GetComponent<Renderer>().material.color;
-                tempcolor.a = 0.5f;
-                GetComponent<Renderer>().material.color = tempcolor;
-
-              //realList.Add((GameObject)obj);
-            }
-        }
-      //  Selection.objects = realList.ToArray();
+      foreach(GameObject go in allObjects){
+        if (go.activeInHierarchy)
+           print(go +" is an active object");
+           Renderer rend =  go.GetComponent<Renderer>();
+           Material material = gameObject.GetComponent<Renderer>().material;
+           tempcolor = gameObject.GetComponent<Renderer>().material.color;
+           tempcolor.a = 0.4f;
+           material.color = tempcolor;
+           rend.material = material;
+      }
     }
 
 }
